7a9d3e89158373fff060b1be11ec2951d316643e
José Manuel Nieto Sánchez <superjmn@outlook.com>
2019-02-16 11:37:56 +0100

    Missed interface update

--- Deployer/Tasks/IZipExtractor.cs
+++ Deployer/Tasks/IZipExtractor.cs
@@ -5,7 +5,8 @@ namespace Deployer.Tasks
 {
     public interface IZipExtractor
     {
-        Task ExtractFirstChildToFolder(Stream stream, string folder);
+        Task ExtractFirstChildToFolder(Stream stream, string folderPath);
         Task ExtractToFolder(Stream stream, string folderPath);
+        Task ExtractRelativeFolder(Stream stream, string relativeZipPath, string destination);
     }
 }
