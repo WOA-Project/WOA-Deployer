d66d5fe72691c63ee01ca1dededa42cd0c7e29a1
José Manuel Nieto Sánchez <superjmn@outlook.com>
2019-02-23 20:22:09 +0100

    Console Embedder and AutoUpdater

---
+++ Deployer.Gui.Common/ConsoleEmbedder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Deployer.Gui.Common
+{
+    public static class ConsoleEmbedder
+    {
+        private const uint WmChar = 0x0102;
+        private const int VkEnter = 0x0D;
+
+        [DllImport("kernel32.dll")]
+        private static extern bool AttachConsole(int dwProcessId);
+
+        private const int AttachParentProcess = -1;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool FreeConsole();
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        private static extern int SendMessage(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+
+        public static void ExecuteInsideConsole(Action consoleAction)
+        {
+            AttachConsole(AttachParentProcess);
+            IntPtr cw = GetConsoleWindow();
+
+            System.Console.WriteLine();
+
+            consoleAction();
+            SendMessage(cw, WmChar, (IntPtr) VkEnter, IntPtr.Zero);
+            FreeConsole();
+        }
+    }
+}
---
+++ Deployer.Gui.Common/UpdateChecker.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using AutoUpdaterDotNET;
+using HtmlAgilityPack;
+using Serilog;
+
+namespace Deployer.Gui.Common
+{
+    public static class UpdateChecker
+    {
+        private static string url;
+
+        public static void CheckForUpdates(string baseUrl)
+        {
+            url = $"{baseUrl}/releases/latest";
+
+            AutoUpdater.ParseUpdateInfoEvent += AutoUpdaterOnParseUpdateInfoEvent;
+
+            AutoUpdater.Start(url);
+        }
+
+        private static void AutoUpdaterOnParseUpdateInfoEvent(ParseUpdateInfoEventArgs args)
+        {
+            try
+            {
+                args.UpdateInfo = GetUpdateInfo(args.RemoteData);
+            }
+            catch (Exception e)
+            {
+                Log.Verbose(e, "Error while checking for updates");
+                args.UpdateInfo = new UpdateInfoEventArgs();
+            }            
+        }
+
+        private static UpdateInfoEventArgs GetUpdateInfo(string argsRemoteData)
+        {
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(argsRemoteData);
+
+            return new UpdateInfoEventArgs
+            {
+                CurrentVersion = GetVersion(htmlDoc),
+                DownloadURL = GetDownload(htmlDoc),
+                ChangelogURL = url,
+            };
+        }
+
+        private static Version GetVersion(HtmlDocument htmlDoc)
+        {
+            var versioNode = htmlDoc.DocumentNode.Descendants("span").First(d =>
+                d.Attributes.Contains("class") && d.Attributes["class"].Value.Equals("css-truncate-target"));
+            var match = Regex.Match(versioNode.InnerText, @"(\*|\d+(\.\d+)*(\.\*)?)", RegexOptions.Singleline);
+            var version = new Version(match.Value);
+            return version;
+        }
+
+        private static string GetDownload(HtmlDocument htmlDoc)
+        {
+            var fileNameNode = htmlDoc.DocumentNode.Descendants("div").First(d =>
+                d.Attributes.Contains("class") && d.Attributes["class"].Value.Equals("d-flex flex-items-start"));
+            var linkNode = fileNameNode.Descendants("a").First();
+
+            var relativeDownloadUrl = linkNode.Attributes["href"].Value;
+            var downloadUrl = "https://github.com" + relativeDownloadUrl;
+            return downloadUrl;
+        }
+    }
+}
