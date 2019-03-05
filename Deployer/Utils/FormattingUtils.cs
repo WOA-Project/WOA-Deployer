610920b5756966c6f59c98b17faf80ffb35971c0
José Manuel Nieto Sánchez <superjmn@outlook.com>
2019-03-05 11:34:59 +0100

    Meaningful exception in GetGuid

--- Deployer/Utils/FormattingUtils.cs
+++ Deployer/Utils/FormattingUtils.cs
@@ -9,7 +9,14 @@ namespace Deployer.Utils
         public static Guid GetGuid(string str)
         {
             var guids = Regex.Matches(str, @"(\{){0,1}[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}(\}){0,1}");
-            var match = guids.Cast<Match>().First(x => x.Success).Value;
+            var guid = guids.Cast<Match>().FirstOrDefault(x => x.Success);
+
+            if (guid == null)
+            {
+                throw new ApplicationException("Cannot retrieve any Guid from the given string");
+            }
+
+            var match = guid.Value;
             return Guid.Parse(match);
         }
     }
