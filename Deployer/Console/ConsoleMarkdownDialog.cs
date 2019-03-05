c5d0d2f2119d910475760a4c4a8aa87bbf7f6a71
José Manuel Nieto Sánchez <superjmn@outlook.com>
2019-03-05 22:36:09 +0100

    Code reuse for console

---
+++ Deployer/Console/ConsoleMarkdownDialog.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Deployer.Console
+{
+    public class ConsoleMarkdownDialog : IMarkdownDialog
+    {
+        public Task<Option> PickOptions(string markdown, IEnumerable<Option> options)
+        {
+            System.Console.WriteLine(
+@"By continuing you are accepting the following license below.
+If you decline it, press Control+C anytime during the deployment process.
+" + markdown);
+            return Task.FromResult(new Option("Accept", DialogValue.OK));
+        }
+    }
+}
