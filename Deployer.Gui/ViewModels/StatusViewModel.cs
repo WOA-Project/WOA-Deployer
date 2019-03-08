37e61f146dedac63ab456b5cc6a503e899af1e6d
José Manuel Nieto Sánchez <superjmn@outlook.com>
2019-03-08 14:28:12 +0100

    OpenLogFolder moved to StatusViewModel

--- Deployer.Gui/ViewModels/StatusViewModel.cs
+++ Deployer.Gui/ViewModels/StatusViewModel.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Reactive;
 using System.Reactive.Linq;
 using System.Threading;
 using DynamicData;
@@ -10,6 +12,7 @@ namespace Deployer.Gui.ViewModels
 {
     public class StatusViewModel : ReactiveObject, IDisposable
     {
+        private readonly IFileSystemOperations fileSystemOperations;
         private readonly ObservableAsPropertyHelper<bool> isProgressIndeterminate;
 
         private ObservableAsPropertyHelper<RenderedLogEvent> statusHelper;
@@ -17,8 +20,9 @@ namespace Deployer.Gui.ViewModels
 
         private ReadOnlyObservableCollection<RenderedLogEvent> logEvents;
 
-        public StatusViewModel(IObservable<LogEvent> events, IObservable<double> progressSubject)
+        public StatusViewModel(IFileSystemOperations fileSystemOperations, IObservable<LogEvent> events, IObservable<double> progressSubject)
         {
+            this.fileSystemOperations = fileSystemOperations;
             progressHelper = progressSubject
                 .Where(d => !double.IsNaN(d))
                 .ToProperty(this, model => model.Progress);
@@ -32,6 +36,14 @@ namespace Deployer.Gui.ViewModels
                 .ToProperty(this, x => x.IsProgressIndeterminate);
 
             SetupLogging(events);
+
+            OpenLogFolder = ReactiveCommand.Create(OpenLogs);
+        }
+
+        private void OpenLogs()
+        {
+            fileSystemOperations.EnsureDirectoryExists("Logs");
+            Process.Start("Logs");
         }
 
         public bool IsProgressIndeterminate => isProgressIndeterminate.Value;
@@ -69,6 +81,7 @@ namespace Deployer.Gui.ViewModels
             conn.Connect();
         }
 
+        public ReactiveCommand<Unit, Unit> OpenLogFolder { get; }
         private static RenderedLogEvent RenderedLogEvent(LogEvent x)
         {
             return new RenderedLogEvent
