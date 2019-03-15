6613178be79093b59c61f0e0a0c9d13801dfde1e
José Manuel Nieto Sánchez <superjmn@outlook.com>
2019-03-15 23:05:59 +0100

    ProgressViewModel

---
+++ Deployer.Gui/ProgressButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Deployer.Gui
+{
+    public class ProgressButton : ContentControl
+    {
+        static ProgressButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ProgressButton), new FrameworkPropertyMetadata(typeof(ProgressButton)));
+        }
+
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register(
+            "Icon", typeof(object), typeof(ProgressButton), new PropertyMetadata(default(object)));
+
+        public object Icon
+        {
+            get { return (object) GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public static readonly DependencyProperty ProgressProperty = DependencyProperty.Register(
+            "Progress", typeof(ProgressViewModel), typeof(ProgressButton), new PropertyMetadata(default(ProgressViewModel)));
+
+        public ProgressViewModel Progress
+        {
+            get { return (ProgressViewModel) GetValue(ProgressProperty); }
+            set { SetValue(ProgressProperty, value); }
+        }
+    }
+}
---
+++ Deployer.Gui/ProgressViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Reactive.Linq;
+using ByteSizeLib;
+using ReactiveUI;
+using Serilog;
+
+namespace Deployer.Gui
+{
+    public class ProgressViewModel : ReactiveObject
+    {
+        public IReactiveCommand Command { get; }
+
+        private readonly ObservableAsPropertyHelper<double> progress;
+        private readonly ObservableAsPropertyHelper<bool> isProgressVisible;
+        private readonly ObservableAsPropertyHelper<bool> isProgressIndeterminate;
+        private readonly ObservableAsPropertyHelper<ByteSize> downloaded;
+        private readonly ObservableAsPropertyHelper<bool> isExecuting;
+
+        public ProgressViewModel(object owner, IReactiveCommand command, IDialog dialog, IDownloadProgress operationProgress)
+        {
+            Command = command;
+            progress = operationProgress.Percentage
+                .Where(d => !double.IsNaN(d))
+                .ToProperty(this, model => model.Progress);
+
+            isProgressVisible = operationProgress.Percentage
+                .Select(d => !double.IsNaN(d))
+                .CombineLatest(command.IsExecuting, (isNumber, isExecuting) => isNumber && isExecuting)
+                .ToProperty(this, x => x.IsProgressVisible);
+
+            isProgressIndeterminate = operationProgress.Percentage
+                .Select(double.IsPositiveInfinity)
+                .ToProperty(this, x => x.IsProgressIndeterminate);
+
+            downloaded = operationProgress.BytesDownloaded
+                .Select(x => ByteSize.FromBytes(x))
+                .Sample(TimeSpan.FromSeconds(1))
+                .ToProperty(this, model => model.Downloaded);
+
+            command.ThrownExceptions.Subscribe(async e =>
+            {
+                Log.Error(e, "An error has occurred");
+                Log.Information($"Error: {e.Message}");
+                await dialog.ShowAlert(owner, Resources.ErrorTitle, $"{e.Message}");
+            });
+
+            isExecuting = command.IsExecuting
+                //.StartWith(false)
+                .ToProperty(this, x => x.IsExecuting);
+        }
+
+        public bool IsExecuting => isExecuting.Value;
+
+        public ByteSize Downloaded => downloaded.Value;
+
+        public bool IsProgressIndeterminate => isProgressIndeterminate.Value;
+
+        public bool IsProgressVisible => isProgressVisible.Value;
+
+        public double Progress => progress.Value;
+    }
+}
