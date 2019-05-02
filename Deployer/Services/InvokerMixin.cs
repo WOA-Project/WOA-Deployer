b2eff53f35d697ce22529ef7395c8763d3ffe050
José Manuel Nieto Sánchez <superjmn@outlook.com>
2019-05-02 19:19:20 +0200

    SafeCreate

---
+++ Deployer/Services/InvokerMixin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Deployer.Services
+{
+    public static class InvokerMixin
+    {
+        public static void SafeCreate(this IBcdInvoker invoker, Guid guid, string args)
+        {
+            if (invoker.Invoke($"/enum {{guid}}").Contains(guid.ToString()))
+            {
+                return;
+            }
+
+            invoker.Invoke($"/create {{{guid}}} {args}");
+        }
+    }
+}
