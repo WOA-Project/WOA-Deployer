f16fdd7949073f6d3272b264fc3b18bb8298baad
José Manuel Nieto Sánchez <superjmn@outlook.com>
2019-05-05 00:48:49 +0200

    Reorganization

--- Deployer/DeviceMixin.cs
+++ Deployer/DeviceMixin.cs
@@ -1,42 +1,16 @@
-﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using ByteSizeLib;
 using Deployer.FileSystem;
-using Serilog;
 
 namespace Deployer
 {
     public static class DeviceMixin
     {
-        private static readonly ByteSize ValidResizeThreshold = ByteSize.FromMegaBytes(200);
-      
         public static async Task<bool> HasEnoughSpace(this IDevice device, ByteSize requiredSize)
         {
             return (await device.GetDeviceDisk()).HasEnoughSpace(requiredSize);
         }
-
-        public static bool HasEnoughSpace(this Disk disk, ByteSize requiredSize)
-        {
-            Log.Verbose("Available {Available}. Required {Required}", disk.AvailableSize, requiredSize);
-
-            if (disk.AvailableSize >= requiredSize)
-            {                
-                Log.Verbose("We have enough available space!");
-                return true;
-            }
-
-            Log.Verbose("We don't have enough space. Checking for tolerable threshold of {Threshold}", ValidResizeThreshold);
-
-            var diff = disk.AvailableSize - requiredSize;
-            var isThereEnoughSpace = Math.Abs(diff.MegaBytes) <= ValidResizeThreshold.MegaBytes;
-
-            Log.Verbose("Available - Required => {Available} - {Required} = {Difference}",disk.AvailableSize, requiredSize, diff);
-            Log.Verbose("Enough space? {Result}", isThereEnoughSpace);
-
-            return isThereEnoughSpace;
-        }
-
+        
         public static async Task<Volume> GetVolumeByPartitionName(this IDevice device, string partitionName)
         {
             var disk = await device.GetDeviceDisk();
--- Deployer/FileSystem/FileSystemMixin.cs
+++ Deployer/FileSystem/FileSystemMixin.cs
@@ -3,11 +3,16 @@ using System.IO;
 using System.Reactive.Linq;
 using System.Reactive.Threading.Tasks;
 using System.Threading.Tasks;
+using ByteSizeLib;
+using Serilog;
 
 namespace Deployer.FileSystem
 {
-    public static class FileSystemMixin2
+    public static class FileSystemMixin
     {
+        private static readonly ByteSize ValidResizeThreshold = ByteSize.FromMegaBytes(200);
+
+
         public static string CombineRelativeBcdPath(this string root)
         {
             return Path.Combine(root, "EFI", "Microsoft", "Boot", "BCD");
@@ -27,5 +32,26 @@ namespace Deployer.FileSystem
 
             return vol;
         }        
+
+        public static bool HasEnoughSpace(this Disk disk, ByteSize requiredSize)
+        {
+            Log.Verbose("Available {Available}. Required {Required}", disk.AvailableSize, requiredSize);
+
+            if (disk.AvailableSize >= requiredSize)
+            {                
+                Log.Verbose("We have enough available space!");
+                return true;
+            }
+
+            Log.Verbose("We don't have enough space. Checking for tolerable threshold of {Threshold}", ValidResizeThreshold);
+
+            var diff = disk.AvailableSize - requiredSize;
+            var isThereEnoughSpace = Math.Abs(diff.MegaBytes) <= ValidResizeThreshold.MegaBytes;
+
+            Log.Verbose("Available - Required => {Available} - {Required} = {Difference}",disk.AvailableSize, requiredSize, diff);
+            Log.Verbose("Enough space? {Result}", isThereEnoughSpace);
+
+            return isThereEnoughSpace;
+        }
     }
 }
