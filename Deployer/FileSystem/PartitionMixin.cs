08cd2438595eba0b761fcf4f267406e6fb88b792
José Manuel Nieto Sánchez <superjmn@outlook.com>
2019-06-01 16:42:20 +0200

    Ensure the Root path isn't null

--- Deployer/FileSystem/PartitionMixin.cs
+++ Deployer/FileSystem/PartitionMixin.cs
@@ -9,6 +9,11 @@ namespace Deployer.FileSystem
             if (partition.Root == null)
             {
                 await partition.AssignDriveLetter();
+
+                if (partition.Root == null)
+                {
+                    throw new FileSystemException($"Could not map the partition {partition} to a drive letter");
+                }
             }
 
             return partition;
