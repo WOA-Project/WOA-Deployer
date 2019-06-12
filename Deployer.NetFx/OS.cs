ff9fe449fb7b92ad2aaf209463e890cb1b1183ac
José Manuel Nieto Sánchez <superjmn@outlook.com>
2019-06-13 00:28:41 +0200

    Added methods to detect the host Windows build.

---
+++ Deployer.NetFx/OS.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Win32;
+
+namespace Deployer.NetFx
+{
+    public class OS
+    {
+        public static bool IsCompatibleWindowsBuild => GetBuildNumber() >= 15063;
+
+        public static int GetBuildNumber()
+        {
+            using (var key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion"))
+            {
+                if (key != null)
+                {
+                    var buildStr = (string)key.GetValue("CurrentBuild");
+                    if (int.TryParse(buildStr, out var buildNumber))
+                    {
+                        return buildNumber;
+                    }
+                }
+            }
+
+            return 0;
+        }
+    }
+}
--- Deployer.UI/Properties/Resources.Designer.cs
+++ Deployer.UI/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Deployer.UI.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -93,6 +93,28 @@ namespace Deployer.UI.Properties {
         public static string ErrorTitle {
             get {
                 return ResourceManager.GetString("ErrorTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Sorry, but your Windows 10 installation is too old to run WOA Deployer. 
+        ///
+        ///This application needs at least Windows 10 Creators Update (15063).
+        ///
+        ///Please, update your system using Windows Update before using it..
+        /// </summary>
+        public static string IncompatibleWindows10Build {
+            get {
+                return ResourceManager.GetString("IncompatibleWindows10Build", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Incompatible Windows 10 build.
+        /// </summary>
+        public static string IncompatibleWindows10BuildTitle {
+            get {
+                return ResourceManager.GetString("IncompatibleWindows10BuildTitle", resourceCulture);
             }
         }
         
