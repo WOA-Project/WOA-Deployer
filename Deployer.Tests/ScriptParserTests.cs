20b6d7ea1d0dd4732314cf1fe097c76fa6471be0
jprat <jprat@kds.com>
2019-06-20 16:14:23 +0200

    fixed new lines script parsing

--- Deployer.Tests/ScriptParserTests.cs
+++ Deployer.Tests/ScriptParserTests.cs
@@ -7,11 +7,33 @@ namespace Deployer.Tests
     public class ScriptParserTests
     {
         [Theory]
+        // Base tests
         [InlineData("Task", "Task()")]
         [InlineData("Task \"Hola\"", "Task(Hola)")]
         [InlineData("Task \"Hola\" \"Cómo vas\"", "Task(Hola,Cómo vas)")]
         [InlineData("# Comment", "# Comment")]
-        public void Test1(string input, string expected)
+        // New lines related tests
+        [InlineData("\nTask", "Task()")]
+        [InlineData(" \nTask", "Task()")]
+        [InlineData("\r\nTask", "Task()")]
+        [InlineData(" \r\nTask", "Task()")]
+        [InlineData("Task\n", "Task()")]
+        [InlineData("Task\n ", "Task()")]
+        [InlineData("Task\r\n", "Task()")]
+        [InlineData("Task\r\n ", "Task()")]
+        [InlineData("Task\n\n", "Task()")]
+        [InlineData("Task\n\n ", "Task()")]
+        [InlineData("Task\r\n\r\n", "Task()")]
+        [InlineData("Task\r\n\r\n ", "Task()")]
+        [InlineData("Task\r\n\r\n\r\n", "Task()")]
+        [InlineData("Task\r\n\r\n\r\n ", "Task()")]
+        [InlineData("Task\r\n \r\n", "Task()")]
+        [InlineData("Task\r\n \r\n ", "Task()")]
+        [InlineData("Task1\r\n \r\nTask2", "Task1()\nTask2()")]
+        [InlineData("Task1 \r\n \r\n Task2", "Task1()\nTask2()")]
+        [InlineData("Task1\r\n\r\n\r\nTask2", "Task1()\nTask2()")]
+        [InlineData("Task1 \r\n \r\n \r\n Task2", "Task1()\nTask2()")]
+        public void Test_Parse(string input, string expected)
         {
             AssertParse(input, expected);
         }
--- Deployer/Execution/ScriptParser.cs
+++ Deployer/Execution/ScriptParser.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 using Superpower;
 
 namespace Deployer.Execution
 {
     public class ScriptParser : IScriptParser
     {
+        private static Regex _emptyLinesRegex = new Regex(@"((\s*\r?\n)+\s*){2,}");
         private readonly Tokenizer<LangToken> tokenizer;
 
         public ScriptParser(Tokenizer<LangToken> tokenizer)
@@ -15,22 +17,10 @@ namespace Deployer.Execution
 
         public Script Parse(string input)
         {
-            input = input.Replace("\r\n", "\n");
+            input = _emptyLinesRegex.Replace(input.Trim(), "\n");
 
             var tokenList = tokenizer.Tokenize(input);
             return Parsers.Script.Parse(tokenList);
-        }
-
-        static IEnumerable<string> Lines(string str)
-        {
-            using (var reader = new StringReader(str))
-            {
-                string line;
-                while ((line = reader.ReadLine()) != null)
-                {
-                    yield return line;
-                }
-            }
         }
     }
 }
