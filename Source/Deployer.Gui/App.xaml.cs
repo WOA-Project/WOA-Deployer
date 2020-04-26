8ea35b7910725ffaa75ae53e3f4d84882b5bf63c
José Manuel Nieto Sánchez <superjmn@outlook.com>
2020-04-26 12:52:22 +0200

    Configure logs for GUI

--- Source/Deployer.Gui/App.xaml.cs
+++ Source/Deployer.Gui/App.xaml.cs
@@ -1,9 +1,20 @@
-﻿namespace Deployer.Gui
+﻿using System.Windows;
+using Serilog;
+using Serilog.Events;
+
+namespace Deployer.Gui
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.RollingFile(@"Logs\Log-{Date}.txt")
+                .MinimumLevel.Debug()
+                .CreateLogger();
+        }
     }
 }
