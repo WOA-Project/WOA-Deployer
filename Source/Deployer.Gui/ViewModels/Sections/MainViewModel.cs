e5b7806af90473f5f98da43ba50658f4cba7b067
José Manuel Nieto Sánchez <superjmn@outlook.com>
2021-01-03 21:42:05 +0100

    Improved show warning command

--- Source/Deployer.Gui/ViewModels/Sections/MainViewModel.cs
+++ Source/Deployer.Gui/ViewModels/Sections/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Reactive;
+using System.Reactive.Linq;
 using Deployer.Core.Interaction;
 using Deployer.Gui.Properties;
 using Deployer.Wpf;
@@ -17,10 +18,12 @@ namespace Deployer.Gui.ViewModels.Sections
         public MainViewModel(IList<Meta<ISection>> sections, IList<IBusy> busies, IInteraction interaction, OperationProgressViewModel operationProgress) : base(sections, busies)
         {
             OperationProgress = operationProgress;
-            ShowWarningCommand = ReactiveCommand.CreateFromTask(() => interaction.Message(Resources.TermsOfUseTitle, Resources.WarningNotice, "OK".Some(), Optional.Option.None<string>()));
+            ShowWarning = ReactiveCommand.CreateFromObservable(() => 
+                Observable.FromAsync(() =>  interaction.Message(Resources.TermsOfUseTitle, Resources.WarningNotice, "OK".Some(),
+                        Optional.Option.None<string>())));
         }
 
-        public ReactiveCommand<Unit, Unit> ShowWarningCommand { get; set; }
+        public ReactiveCommand<Unit, Unit> ShowWarning { get; set; }
 
         protected override string DonationLink => "https://github.com/WoA-project/WOA-Deployer/blob/master/Docs/Donations.md";
         protected override string HelpLink => "https://github.com/WOA-Project/WOA-Deployer-Lumia#need-help";
