da98b6d68125f0649d2419c6513edee57e0570cd
José Manuel Nieto Sánchez <superjmn@outlook.com>
2021-01-10 15:24:26 +0100

    Added superfloppy workaround

--- Source/Deployer.Filesystem/DiskMixin.cs
+++ Source/Deployer.Filesystem/DiskMixin.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
 using ByteSizeLib;
+using Deployer.Filesystem.Gpt;
 
 namespace Deployer.Filesystem
 {
@@ -56,6 +58,27 @@ namespace Deployer.Filesystem
                 select p;
 
             return matching.FirstOrDefault();
+        }
+
+        public static void ConfigureAsSuperFloppy(IDisk disk)
+        {
+            using (var deviceStream = new DeviceStream("\\\\.\\PhysicalDrive" + disk.Number, FileAccess.ReadWrite))
+            {
+                var buffer1 = new byte[87];
+                deviceStream.Seek(0x100000, SeekOrigin.Begin);
+                deviceStream.Read(buffer1, 0, buffer1.Length);
+                Buffer.BlockCopy(BitConverter.GetBytes((short)(BitConverter.ToInt16(new[]
+                {
+                    buffer1[14],
+                    buffer1[15]
+                }, 0) + 2048)), 0, buffer1, 14, 2);
+                var buffer2 = new byte[512];
+                deviceStream.Seek(0L, SeekOrigin.Begin);
+                deviceStream.Read(buffer2, 0, buffer2.Length);
+                Buffer.BlockCopy(buffer1, 11, buffer2, 11, 76);
+                deviceStream.Seek(0L, SeekOrigin.Begin);
+                deviceStream.Write(buffer2, 0, buffer2.Length);
+            }
         }
     }
 }
